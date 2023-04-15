55cff2d055b0d2d39857b251fdd6d88280f2eeeb
Kosmic Teal <69432458+kosmicteal@users.noreply.github.com>
2023-04-15 10:09:53 +0200

    Recommited master branch

---
+++ README.md
@@ -0,0 +1,17 @@
+# VocalSynthPlugins
+![](https://img.shields.io/badge/number%20of%20plugins-3-lightgrey) ![](https://img.shields.io/badge/intended%20for-VOCALOID3-FFFFFF)![](https://img.shields.io/badge/-VOCALOID4-000000)
+![alt text](https://github.com/kosmicteal/VocalSynthPlugins/blob/main/02_VocalSynthPlugins.png)
+
+Plugins for various Signing Voice Synthesizers that allow scripting
+
+## V○CAL○ID3/4 (.lua)
+**How to install them?**: Download your desired .lua files and drop them in (Program Files)\V○CAL○IDX\Editor\J○bPlugins (where X is your editor version), then in V3/V4 go to Job(J) -> Manage Job Plugins (M)
+* **ListKeyNotes**: Create same-length notes in a specific key
+  * **How it works?**: Set the Position wherever you want the notes and run the Plug-in. Everything else is self-explanatory.
+* **Crash-B-Gone for Tonio**: Replaces all @ for V
+  * **How it works?**: Choose a part in a VSQx and run the Plug-in.
+* **VocaAutoPitch (April Fools 2021)**: Adds the first four notes of Toby Fox's Megalovania from UNDERTALE.
+  * **How it works?**: Put the marker where you want the notes, the only parameter that is taken in count while selecting is "Language"
+
+# Note
+As we don't know what's going to happen in the future with this synth or its compatibilities with future Windows OS, always make a backup of the file before running any plugin.
---
+++ vocaldroid/ListKeyNotes.lua
@@ -0,0 +1,199 @@
+--
+-- ListKeyNotes
+
+--
+-- KosmicTeal (https://github.com/KosmicTeal/)
+-- MAIKA fan since 2013, Voctro Labs' fan since 2011 and
+-- waiting for new Spanish commercial voicebanks since 2013 too
+--
+
+------------- MANIFEST
+function manifest()
+    myManifest = {
+        name          = "ListKeyNotes",
+        comment       = "Create same-length notes in a specific key",
+        author        = "KosmicTeal",
+        pluginID      = "{DB95F4DD-78DB-FA6E-7FE1-E45D77335FB5}",
+        pluginVersion = "1.1.0.0",
+        apiVersion    = "3.0.0.1"
+    }
+    
+    return myManifest
+end
+
+------------- CONVERSION SETS FROM STRINGS TO DATA
+keyTypeListMap = {
+	{ keyTypeStr = "C",	keyType = 0  },  
+	{ keyTypeStr = "D",	keyType = 1  },  
+	{ keyTypeStr = "E",	keyType = 2  },  
+	{ keyTypeStr = "F",	keyType = 3  },  
+	{ keyTypeStr = "G",	keyType = 4  }, 
+	{ keyTypeStr = "A",	keyType = 5  },  
+	{ keyTypeStr = "B",	keyType = 6  }
+}
+keyTypeIDNum = table.getn(keyTypeListMap)
+
+signTypeListMap = {
+	{ signTypeStr = " ",	signType = 0  }, 
+	{ signTypeStr = "♯",	signType = 1  },
+	{ signTypeStr = "♭", 	signType = -1  }
+}
+singTypeIDNum = table.getn(signTypeListMap)
+
+lengTypeListMap = {
+	{ lengTypeStr = "1/4 (Default)",	lengType = 480  },  
+	{ lengTypeStr = "1/1",	lengType = 1920  }, 
+	{ lengTypeStr = "1/2",	lengType = 960  },  
+	{ lengTypeStr = "1/8",	lengType = 240  },  
+	{ lengTypeStr = "1/16",	lengType = 120  },  
+	{ lengTypeStr = "1/32",	lengType = 60  },
+	{ lengTypeStr = "1/64",	lengType = 30  },
+}
+lengTypeIDNum = table.getn(lengTypeListMap)
+
+function main(processParam, envParam)	
+
+	----------------------------------------------------- Main initalizer
+	local beginPosTick = processParam.beginPosTick
+	local endPosTick   = processParam.endPosTick
+	local songPosTick  = processParam.songPosTick
+
+	local scriptDir  = envParam.scriptDir
+	local scriptName = envParam.scriptName
+	local tempDir    = envParam.tempDir
+	
+	----------------------------------------------------- UI creation
+	VSDlgSetDialogTitle("ListKeyNotes - Create same-length notes in a specific key") 
+
+	local field = {}
+
+
+	field.name       = "key"
+	field.caption    = "Key"
+	field.initialVal =
+		"C" ..
+		", D" ..
+		", E" ..
+		", F" ..
+		", G" ..
+		", A" ..
+		", B" 
+	field.type = 4				-- table (0=int, 1=bool, 3=string, 4=table)
+	dlgStatus  = VSDlgAddField(field)
+	
+	field.name       = "sign"
+	field.caption    = "Signature"
+	field.initialVal =
+		" " ..
+		", ♯" ..
+		", ♭"
+	field.type = 4
+	dlgStatus  = VSDlgAddField(field)
+	
+	field.name       = "value"
+	field.caption    = "Value (between -2 and 8)"
+	field.initialVal = "3"
+    field.type       = 0
+	dlgStatus  = VSDlgAddField(field)
+	
+	field.name       = "leng"
+	field.caption    = "Length"
+	field.initialVal =
+		"1/4 (Default)" ..
+		", 1/1" ..
+		", 1/2" ..
+		", 1/8" ..
+		", 1/16" ..
+		", 1/32" ..
+		", 1/64"
+	field.type = 4
+	dlgStatus  = VSDlgAddField(field)
+	
+	field.name       = "number"
+	field.caption    = "Number of notes"
+	field.initialVal = "1"
+	field.type = 0				-- integer
+	dlgStatus  = VSDlgAddField(field)
+	
+	
+
+	dlgStatus = VSDlgDoModal()
+	if (dlgStatus == 2) then
+		-- When it was cancelled.
+		return 0
+	end
+	if ((dlgStatus ~= 1) and (dlgStatus ~= 2)) then
+		-- When it returned an error.
+		return 1
+	end
+
+
+	-- tell software to recollect data
+	dlgStatus, keyTypeStr  = VSDlgGetStringValue("key")
+	dlgStatus, signTypeStr  = VSDlgGetStringValue("sign")
+	dlgStatus, valueNote  = VSDlgGetIntValue("value")
+	dlgStatus, lengTypeStr  = VSDlgGetStringValue("leng")
+	dlgStatus, numberNotes  = VSDlgGetIntValue("number")
+
+	-- recollect data (Strings)
+	local index = 1
+	local keyType = 0
+	for index, str in ipairs(keyTypeListMap) do
+		if (keyTypeStr == str.keyTypeStr) then
+			keyType = str.keyType
+		end
+	end
+	
+	index = 1
+	local signType = 0
+	for index, str in ipairs(signTypeListMap) do
+		if (signTypeStr == str.signTypeStr) then
+			signType = str.signType
+		end
+	end
+	
+	index = 1
+	local lengType = 0
+	for index, str in ipairs(lengTypeListMap) do
+		if (lengTypeStr == str.lengTypeStr) then
+			lengType = str.lengType
+		end
+	end
+
+	-- end query
+	local endStatus = 0
+	
+	if (keyType >= 3) then
+		keyType = keyType - 1
+		signType = signType + 1
+	end
+	resultNotePlace = 12*(valueNote + 2) + keyType * 2 + signType
+	
+	-- Debug stuff
+	VSMessageBox("Key: " .. keyType .. ". Sign: " .. signType .. ". Value: " .. valueNote .. ". \n Duration:" .. lengType .. ".\n Total Place (C3 is 60):" .. resultNotePlace .. "\n Number of notes:" .. numberNotes, 0)
+
+	----------------------------------------------------- Generation
+	
+	-- Note to be repeated
+	
+	local	note = {}                   -- data
+	note.posTick  = songPosTick         -- position where it should be placed
+	note.durTick  = lengType            -- duration
+	note.noteNum  = resultNotePlace     -- which tone is it
+	note.velocity = 64             		
+	note.lyric    = "na"       
+	note.phonemes = "n a"    
+	
+	-- Loop
+	local i = 0
+	while i < numberNotes do 
+		note.posTick = songPosTick + (lengType * i)
+		retCode = VSInsertNote(note)
+		i = i + 1 --LUA DOESN'T LET YOU DO I++ WHAT THE FRICK
+		if (retCode ~= 1) then
+			VSMessageBox("There's been an error while creating the note!", 0)
+			return 1
+		end
+	end
+	
+end
